f52b9737c4f27f1437a58895121afea93fbd8a11
Haung bacon <haungbacon@Haungde-MacBook-Air.local>
2019-01-16 01:19:52 +0800

    purchase function add

--- front_end/js/javascript_purchase.js
+++ front_end/js/javascript_purchase.js
@@ -1,63 +1,6 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-
-  Display_stock_data()
-
-
-  /**
-   * Chosen: Multiple Dropdown
-   */
-  window.WDS_Chosen_Multiple_Dropdown = {};
-  (function(window, $, that) {
-
-    // Constructor.
-    that.init = function() {
-      that.cache();
-
-      if (that.meetsRequirements) {
-        that.bindEvents();
-      }
-    };
-
-    // Cache all the things.
-    that.cache = function() {
-      that.$c = {
-        window: $(window),
-        theDropdown: $('.dropdown'),
-      };
-    };
-
-    // Combine all events.
-    that.bindEvents = function() {
-      that.$c.window.on('load', that.applyChosen);
-    };
-
-    // Do we meet the requirements?
-    that.meetsRequirements = function() {
-      return that.$c.theDropdown.length;
-    };
-
-    // Apply the Chosen.js library to a dropdown.
-    // https://harvesthq.github.io/chosen/options.html
-    that.applyChosen = function() {
-      that.$c.theDropdown.chosen({
-        inherit_select_classes: true,
-        width: '300px',
-      });
-    };
-
-    // Engage!
-    $(that.init);
-
-  })(window, jQuery, window.WDS_Chosen_Multiple_Dropdown);
-
-  $('.form-control-chosen').chosen({
-    // Chosen options here
-  });
-=======
   Display_stocks_data()
->>>>>>> 046fc41dc981bc54a968f5e33b9434d356d08833
-
+  Search_all_purchase()
   // 處理消費者按鈕
   $(document).on("click", "#customer-confirm", function() {
     var empty = false;
@@ -76,18 +19,16 @@ $(document).ready(function() {
     // input telephone name -> 去 customers collection 的 name 和 telephone欄位做比較 -> output "new" "old"
 
   });
-<<<<<<< HEAD
-});
 
 
-
-function Display_stock_data() {
-=======
+  $(document).on("click", ".delete", function(){
+    var tr_handle = $(this).closest('tr');
+    tr_handle.remove();
+  })
 })
 
 
 function Display_stocks_data() {
->>>>>>> 046fc41dc981bc54a968f5e33b9434d356d08833
   $.ajax("http://34.226.147.247:3000/stocks/shelf", {
     type: 'GET',
     success: function(result) {
@@ -97,27 +38,6 @@ function Display_stocks_data() {
       }
 
       var iname_set = GetUnique(iname)
-<<<<<<< HEAD
-
-      var select_form = document.createElement("select");
-      select_form.setAttribute("data-placeholder", "請選擇購買商品，可複選");
-      select_form.setAttribute("name", "tags[]");
-      select_form.setAttribute("class", "chosen-select");
-      select_form.setAttribute("multiple", "multiple");
-
-
-      for (j = 0; j < iname_set.length; j++) {
-        var opt = document.createElement("option");
-        opt.setAttribute("value", iname_set[j])
-        opt.appendChild(document.createTextNode(iname_set[j]));
-        select_form.appendChild(opt)
-        console.log(select_form)
-      }
-      document.getElementById("stock-form").appendChild(select_form);
-
-      document.getElementById('output').innerHTML = location.search;
-      $(".chosen-select").chosen();
-=======
       var select_form = document.createElement("select");
       select_form.setAttribute("data-placeholder", "高山青茶")
       select_form.setAttribute("class", "chosen-select");
@@ -156,7 +76,6 @@ function Display_stocks_data() {
 
       console.log('hello')
 
->>>>>>> 046fc41dc981bc54a968f5e33b9434d356d08833
 
     },
     error: function(jqXHR, textStatus, errorThrown) {
@@ -174,4 +93,138 @@ function GetUnique(inputArray) {
     }
   }
   return outputArray;
+}
+
+function Search_all_purchase() {
+  $(document).on("click", "#customer-select", function() {
+    try{
+        document.getElementsByClassName("table-wrappers").remove();
+    }catch(err){
+      console.log('error')
+    }
+
+    var select_items = $(".chosen-choices").find("span")
+    var search_inames = [];
+    for (i = 0; i < select_items.length; i++) {
+      var iname = $(".chosen-choices").find("span").get(i).innerText
+      search_inames.push(iname)
+    }
+
+
+    $.ajax("http://34.226.147.247:3000/items/shelf", {
+      type: 'GET',
+      success: function(result) {
+        // Create table
+        var table = document.createElement("table");
+        table.setAttribute("class", "table table-bordered")
+
+        var thead = document.createElement("thead");
+        var tbody = document.createElement("tbody");
+        var headRow = document.createElement("tr");
+
+        var HeadName = ["商品名稱", "進貨日期", "單位", "價錢", "份數", "行為"]
+        var AttributesClass = ["col col1 h6", "col col2 h6", "col col3 h6", "col col4 h6", "col col5 h6", "col col6 h6"]
+        var AttributesDataColumn = ["col1", "col2", "col3", "col4", "col5", "col6"]
+
+        for (i = 0; i < 6; i++) {
+          var th = document.createElement("th");
+          th.appendChild(document.createTextNode(HeadName[i]));
+          th.setAttribute("class", AttributesClass[i]);
+          th.setAttribute("class", "h2");
+          th.setAttribute("data-column", AttributesDataColumn[i]);
+          headRow.appendChild(th);
+        }
+        thead.appendChild(headRow);
+        table.appendChild(thead);
+
+        var tBody = document.createElement("tbody");
+
+        var len = result.length;
+        for (var i = 0; i < len; i++) {
+
+          var tr = document.createElement("tr");
+          for (var j = 0; j < 6; j++) {
+            if (!search_inames.includes(result[i].iname)) {
+              continue
+            }
+
+            var td = document.createElement("td");
+
+            if (j == 0) {
+              td.appendChild(document.createTextNode(result[i].iname));
+            }
+
+            if (j == 1) {
+              td.appendChild(document.createTextNode(result[i].date));
+            }
+
+            if (j == 2) {
+              td.appendChild(document.createTextNode(result[i].unit));
+            }
+
+            if (j == 3) {
+              td.appendChild(document.createTextNode(result[i].price));
+            }
+
+            if (j == 4) {
+              var div_form = document.createElement("div")
+              var input_form = document.createElement("form")
+              var span_form = document.createElement("span")
+              var input = document.createElement("input");
+
+              input.setAttribute("type", "text")
+              input.setAttribute("id", "amount")
+              span_form.setAttribute("class", "td")
+              input_form.setAttribute("class", "tr")
+              div_form.setAttribute("class", "table")
+
+              span_form.appendChild(input)
+              input_form.append(span_form)
+              div_form.append(input_form)
+              td.appendChild(div_form);
+            }
+
+
+            if (j == 5) {
+              var para = ["delete"];
+              var icon = ["\uE872"];
+
+              for (k = 0; k < 1; k++) {
+                var but = document.createElement("button");
+                but.setAttribute("class", "h3 delete");
+                but.setAttribute("type", "button");
+
+                var ii = document.createElement("i");
+                // var special_sign = document.createTextNode("")
+                // special_sign.innerHTML = icon[k] + special_sign.innerHTML;
+                // ii.appendChild(special_sign.innerHTML);
+                ii.appendChild(document.createTextNode(icon[k]))
+                ii.setAttribute("class", "material-icons");
+
+                but.appendChild(ii)
+                td.appendChild(but)
+              }
+            }
+
+            td.setAttribute("class", AttributesClass[j]);
+            td.setAttribute("class", "h3");
+            td.setAttribute("data-column", AttributesDataColumn[j]);
+            tr.appendChild(td);
+          }
+          tBody.appendChild(tr);
+        }
+        table.appendChild(tBody);
+
+
+        document.getElementById("table-wrappers").appendChild(table)
+
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+        alert(textStatus, errorThrown);
+      }
+    });
+
+
+  })
 }
