12a5fd5b745e0d5e1414b5168d5e60ffb0f35b43
Haung bacon <haungbacon@Haungde-MacBook-Air.local>
2019-01-26 17:37:02 +0800

    v4

--- back_end/server.js
+++ back_end/server.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:30000/Tea_platform', {
+mongoose.connect('mongodb://localhost:29876/Tea_platform', {
   useNewUrlParser: true
 });
 var express = require('express');
